08ffa70c36cb356a8c6b9dbcf3b2c1089ffa6358
Eddy <eddywei@graduate.utm.my>
2024-06-09 15:00:21 +0800

    add comment

--- mainbody.java
+++ mainbody.java
@@ -12,15 +12,18 @@ public class mainbody {
         int type;
         boolean isValid = true;
 
+        //Get owner name
         System.out.println("-- Vehicle Sticker Application --");
         System.out.print("Please enter vehicle owner's name: ");
         name = input.nextLine();
         
-        do{
+        do{ //Looping for checking validity of IC number
+            //Get IC number
             System.out.print("Please enter your IC(010101010101): ");
             ic = input.nextLine();
             System.out.println();
 
+            //Check validity of IC number - does use enter letters or not
             for (int i = 0; i < ic.length(); i++) {
                 isValid = Character.isDigit(ic.charAt(i));
                 if (isValid == false) {
@@ -29,10 +32,12 @@ public class mainbody {
                 }
             }
 
+            //If not valid, directly looping again to get IC number
             if(isValid == false){
                 continue;
             }
 
+            //Check the length of IC number
             if (ic.length() != 12) {
                 System.out.println("Please provide a valid IC number\n");
                 isValid = false;
@@ -48,7 +53,8 @@ public class mainbody {
 
         boolean validType; //Use to check the validity of type
         
-        do{
+        do{ //Looping for checking the validity of type of vehicle
+            //Get vehicle type
             System.out.println("What type of vehicle do you need to register?");
             System.out.println("1. Motorbike");
             System.out.println("2. Car");
@@ -59,14 +65,17 @@ public class mainbody {
 
             validType = true ;
 
+            //Check the validity of vehicle type
             if (type < 1 || type > 2) {
                 System.out.println("Invalid. Please choose again...\n");
                 validType = false;
             } 
         }while (validType == false);
 
+        //Type 1: Motorbike, Type 2(default): Car
         switch (type) {
             case 1: {
+                //Get motorbike details
                 System.out.print("Please enter the motorbike model (Yamaha): ");
                 model = input.nextLine();
                 System.out.print("Please enter the motorbike brand (MT-15): ");
@@ -75,6 +84,7 @@ public class mainbody {
                 plateNumber = input.nextLine();
                 System.out.println();
 
+                //Get lisence number
                 do{
                     System.out.println("Please provide your license number (12345678)");
                     System.out.println("(The license number is located at the upper right corner of the back side of the card)");
@@ -103,8 +113,9 @@ public class mainbody {
                 }while (isValid == false);
 
                 Motorbike userMotor = new Motorbike(name, ic, model, brand, plateNumber, license);
-                userMotor.calcPrice();
+                userMotor.calcPrice(); //Motorbike price - RM 5
 
+                //Inform user the details that he insert
                 System.out.println("--Your Details--\n");
                 System.out.println("Name: " + userMotor.getOwner());
                 System.out.println("IC number: " + userMotor.getIC());
@@ -114,8 +125,9 @@ public class mainbody {
                 System.out.println("License number: " + userMotor.getLicense());
                 System.out.println("Price need to pay: RM" + userMotor.getPriceNum() + "\n");
 
-                userMotor.payment();
+                userMotor.payment(); //for payment purpose
 
+                //Store the details into a file
                 try (BufferedWriter file = new BufferedWriter(new FileWriter("vehicleInfo.txt", true))) {
                     file.write(userMotor.getOwner() + "\t" +
                                userMotor.getIC() + "\t" +
@@ -131,6 +143,7 @@ public class mainbody {
             }
         
             default: {
+                //Get car details
                 System.out.print("Please enter the car model (Proton): ");
                 model = input.nextLine();
                 System.out.print("Please enter the car brand (Saga): ");
@@ -139,6 +152,7 @@ public class mainbody {
                 plateNumber = input.nextLine();
                 System.out.println();
 
+                //Get license number
                 do{
                     System.out.println("Please provide your license number (12345678)");
                     System.out.println("(The license number is located at the upper right corner of the back side of the card)");
@@ -167,8 +181,9 @@ public class mainbody {
                 }while(isValid == false);
 
                 Car userCar = new Car(name, ic, model, brand, plateNumber, license);
-                userCar.calcPrice();
+                userCar.calcPrice(); //Car price - RM 10
 
+                //Inform user the details that he insert
                 System.out.println("--Your Details--\n");
                 System.out.println("Name: " + userCar.getOwner());
                 System.out.println("IC number: " + userCar.getIC());
@@ -178,8 +193,9 @@ public class mainbody {
                 System.out.println("License number: " + userCar.getLicense());
                 System.out.println("Price need to pay: RM" + userCar.getPriceNum() + "\n");
 
-                userCar.payment();
+                userCar.payment(); //for payment purpose
 
+                //Store the details into a file
                 try (BufferedWriter file = new BufferedWriter(new FileWriter("vehicleInfo.txt", true))) {
                     file.write(userCar.getOwner() + "\t" +
                                userCar.getIC() + "\t" +
