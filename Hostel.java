7d206e09ad74849e2ce74e8829432a8f285225c6
Liow Chee Suan <cheesuanliow@gmail.com>
2024-06-17 03:05:08 +0800

    payment & hostel comment

--- Hostel.java
+++ Hostel.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 abstract class Hostel {
     protected String block;
-    protected String[] matric;
+    protected String[] matric; //array of matric number to solve the problem of having two occupants in a double room
     protected int roomNumber;
 
     public Hostel(String block, int roomNumber, String[] matric) {
@@ -15,10 +15,10 @@ abstract class Hostel {
     public abstract int getRoomNum();
     public abstract String getMatric(int index);
     public abstract void setMatric(String m);
-    public abstract boolean isAvailable();
-    public abstract void setPayment(int type);
-    public abstract void makePayment(Scanner inp);
-    public abstract boolean register(Scanner inp);
+    public abstract boolean isAvailable(); // to check the room is now full or not
+    public abstract void setPayment(int type); // initialize the payment type
+    public abstract void makePayment(Scanner inp);// user decide which type of payment to use, and continue payment
+    public abstract boolean register(Scanner inp);// user register the room by enter the room number and block they want
     public abstract String toString();
 }
 
@@ -44,26 +44,29 @@ class Single_Room extends Hostel {
     }
 
     public void setMatric(String m) {
+        //store the student matric number after they register a room
         matric[0] = m;
     }
 
-    public boolean isAvailable() {
+    public boolean isAvailable() { 
+        //if the available data stored in file is greater then 1, return true 
+        //true means the room is not full yet
         return (available > 0);
     }
 
     public void setPayment(int type) {
-        double amount = 6 * 3 * 30;
+        double amount = 6 * 3 * 30; //RM6 per day * 3 months and each month count for 30 days
         if(type == 1) {
-            payment = new TnG(amount);
+            payment = new TnG(amount);// user choose to use TnG to make payment
         }
         else if(type == 2) {
-            payment = new OnlineBanking(amount);
+            payment = new OnlineBanking(amount);// user choose to use Online Banking to make payment
         }
         else if(type == 3) {
-            payment = new CreditCard(amount);
+            payment = new CreditCard(amount);// user choose to use Credit Card to make payment
         }
         else {
-            payment = new VISA(amount);
+            payment = new VISA(amount);// user choose to use VISA to make payment
         }
     }
 
@@ -74,26 +77,27 @@ class Single_Room extends Hostel {
         System.out.println("3. Credit Card");
         System.out.println("4. VISA");
         System.out.print("\nType : ");
-        int type = inp.nextInt(); 
+        int type = inp.nextInt(); //user enter which type they want to use to make payment
         inp.nextLine();
     
-        setPayment(type);
-        System.out.println("Total need to pay : RM" + payment.getTotal());
+        setPayment(type);// set the payment type
+        System.out.println("Total need to pay : RM" + payment.getTotal()); //display the total amount of payment to user
 
-        payment.paymentProccess(inp);
+        // continue to do the payment proccess
+        payment.paymentProccess(inp); // user enter their payment detail to complete the payment
     }
 
     public boolean register(Scanner inp) {
-        if(available > 0) {
+        if(available > 0) { //when the room is available to rent, set the room to unavailable
             available--;
         }
-        else return false;
+        else return false; //when the room is not available, tell tenant fail to register
 
-        makePayment(inp);
-        return true;
+        makePayment(inp);//after set the room to unavailable, user should continue to make payment
+        return true; //after make payment, tenant registration successful
     }
 
-    public String toString() {
+    public String toString() {// in order to simpifly the single room output display and easy to store into file
         return (block + ", " + roomNumber + ", " + available + ", " + matric[0]);
     }
 }
@@ -120,7 +124,9 @@ class Double_Room extends Hostel {
     }
 
     public void setMatric(String m) {
-        if(matric[0].equals("-")){
+        //store the student matric number after they register a room
+        //check whether the first bunk or second bunk is empty
+        if(matric[0].equals("-")){ // "-" means the bunk still empty
             matric[0] = m;
         }
         else {
@@ -129,22 +135,25 @@ class Double_Room extends Hostel {
     }
 
     public boolean isAvailable() {
+        //if the available data stored in file is greater then 1, return true 
+        //check twice because double room can content 2 students
+        //true means the room is not full yet
         return (available[0] > 0 || available[1] > 0);
     }
 
     public void setPayment(int type) {
-        double amount = 4 * 3 * 30;
+        double amount = 4 * 3 * 30; //RM6 per day * 3 months and each month count for 30 days
         if(type == 1) {
-            payment = new TnG(amount);
+            payment = new TnG(amount); // user choose to use TnG to make payment
         }
         else if(type == 2) {
-            payment = new OnlineBanking(amount);
+            payment = new OnlineBanking(amount);// user choose to use Online Banking to make payment
         }
         else if(type == 3) {
-            payment = new CreditCard(amount);
+            payment = new CreditCard(amount);// user choose to use Credit Card to make payment
         }
         else {
-            payment = new VISA(amount);
+            payment = new VISA(amount);// user choose to use VISA to make payment
         }
     }
 
@@ -155,29 +164,30 @@ class Double_Room extends Hostel {
         System.out.println("3. Credit Card");
         System.out.println("4. VISA");
         System.out.print("\nType : ");
-        int type = inp.nextInt(); 
+        int type = inp.nextInt(); //user enter which type they want to use to make payment
         inp.nextLine();
     
-        setPayment(type);
-        System.out.println("Total need to pay : RM" + payment.getTotal());
+        setPayment(type);// set the payment type
+        System.out.println("Total need to pay : RM" + payment.getTotal());//display the total amount of payment to user
 
-        payment.paymentProccess(inp);
+        // continue to do the payment proccess
+        payment.paymentProccess(inp);// user enter their payment detail to complete the payment
     }
 
     public boolean register(Scanner inp) {
-        if(available[0] > 0) {
+        if(available[0] > 0) {//when the first room bunk is available to rent, set the bunk to unavailable
             available[0]--;
         }
-        else if(available[1] > 0) {
+        else if(available[1] > 0) {//when the second room bunk is available to rent, set the bunk to unavailable
             available[1]--;
         }
-        else return false;
+        else return false;//when the room is not available, tell tenant fail to register
 
-        makePayment(inp);
-        return true;
+        makePayment(inp);//after set the room to unavailable, user should continue to make payment
+        return true;//after make payment, tenant registration successful
     }
 
-    public String toString() {
+    public String toString() {// in order to simpifly the double room output display and easy to store into file
         return (block + ", " + roomNumber + ", " + available[0] + ", " + available[1] + ", " + matric[0] + ", " + matric[1]);
     }
 }
--- Payment.java
+++ Payment.java
@@ -23,12 +23,12 @@ class TnG implements Payment {
     public void paymentProccess(Scanner inp) {
         boolean phoneNumFormat = false;
 
-        while(!phoneNumFormat) {
+        while(!phoneNumFormat) { //when the phone number format is incorrect, repeat this loop
             System.out.println("Please enter your phone number");
             System.out.print(  "> ");
             phoneNum = inp.nextLine();
 
-            if(phoneNum.length() == 11 || phoneNum.length() == 12) {
+            if(phoneNum.length() == 11 || phoneNum.length() == 12) { //check the phone number format
                 for(int i = 0; i < phoneNum.length(); i++) {
                     if(i != 3) {
                         phoneNumFormat = Character.isDigit(phoneNum.charAt(i));
@@ -39,15 +39,15 @@ class TnG implements Payment {
                 }
             }
 
-            if(!phoneNumFormat) {
+            if(!phoneNumFormat) {//if the phone number format is incorrect, tell user that the format is error
                 System.out.println("Format Error! Please type again...");
             }
         }
 
-        printReceipt();
+        printReceipt();//after the format correct, print the receipt to payer
     }
 
-    public void printReceipt() {
+    public void printReceipt() { //print all data related to payment details
         System.out.println("--Your Payment Detail--");
         System.out.println("TnG Number: " + phoneNum);
         System.out.println("Total payment: RM " + amount + "\n");
@@ -72,7 +72,7 @@ class OnlineBanking implements Payment {
     public void paymentProccess(Scanner inp) {
         boolean accountNumFormat = false;
 
-        while(!accountNumFormat) {
+        while(!accountNumFormat) {//when the bank account number format is incorrect, repeat this loop
             System.out.println("Please enter your account number");
             System.out.print(  "> ");
             accountNum = inp.nextLine();
@@ -81,21 +81,21 @@ class OnlineBanking implements Payment {
             System.out.print(  "> ");
             bankName = inp.nextLine();
 
-            if(accountNum.length() >= 12 && accountNum.length() <= 16) {
+            if(accountNum.length() >= 12 && accountNum.length() <= 16) {//check the account number format
                 for(int i = 0; i < accountNum.length(); i++) {
                     accountNumFormat = Character.isDigit(accountNum.charAt(i));
                 }
             }
 
-            if(!accountNumFormat) {
+            if(!accountNumFormat) {//if the account number format is incorrect, tell user that the format is error
                 System.out.println("Format Error! Please type again...");
             }
         }
 
-        printReceipt();
+        printReceipt();//after the format correct, print the receipt to payer
     }
 
-    public void printReceipt() {
+    public void printReceipt() {//print all data related to payment details
         System.out.println("--Your Payment Detail--");
         System.out.println("Bank: " + bankName);
         System.out.println("Account Number: " + accountNum);
@@ -121,26 +121,26 @@ class CreditCard implements Payment {
     public void paymentProccess(Scanner inp) {
         boolean cardNumFormat = false;
 
-        while(!cardNumFormat) {
+        while(!cardNumFormat) {//when the credit card number format is incorrect, repeat this loop
             System.out.println("Please enter your phone number");
             System.out.print(  "> ");
             Credit_cardNum = inp.nextLine();
 
-            if(Credit_cardNum.length() == 16) {
+            if(Credit_cardNum.length() == 16) {//check the credit card number format
                 for(int i = 0; i < Credit_cardNum.length(); i++) {
                     cardNumFormat = Character.isDigit(Credit_cardNum.charAt(i));
                 }    
             }
             
-            if(!cardNumFormat) {
+            if(!cardNumFormat) {//if the credit number format is incorrect, tell user that the format is error
                 System.out.println("Format Error! Please type again...");
             }
         }
 
-        printReceipt();
+        printReceipt();//after the format correct, print the receipt to payer
     }
 
-    public void printReceipt() {
+    public void printReceipt() {//print all data related to payment details
         System.out.println("--Your Payment Detail--");
         System.out.println("Credit Card Number: " + Credit_cardNum);
         System.out.println("Total payment: RM " + amount + "\n");
@@ -163,26 +163,26 @@ class VISA implements Payment {
     public void paymentProccess(Scanner inp) {
         boolean cardNumFormat = false;
 
-        while(!cardNumFormat) {
+        while(!cardNumFormat) {//when the VISA number format is incorrect, repeat this loop
             System.out.println("Please enter your phone number");
             System.out.print(  "> ");
             VISA_cardNum = inp.nextLine();
 
-            if(VISA_cardNum.length() == 16) {
+            if(VISA_cardNum.length() == 16) {//check the VISA card number format
                 for(int i = 0; i < VISA_cardNum.length(); i++) {
                     cardNumFormat = Character.isDigit(VISA_cardNum.charAt(i));
                 }    
             }
             
-            if(!cardNumFormat) {
+            if(!cardNumFormat) {//if the VISA number format is incorrect, tell user that the format is error
                 System.out.println("Format Error! Please type again...");
             }
         }
 
-        printReceipt();
+        printReceipt();//after the format correct, print the receipt to payer
     }
 
-    public void printReceipt() {
+    public void printReceipt() {//print all data related to payment details
         System.out.println("--Your Payment Detail--");
         System.out.println("VISA Card Number: " + VISA_cardNum);
         System.out.println("Total payment: RM " + amount + "\n");
